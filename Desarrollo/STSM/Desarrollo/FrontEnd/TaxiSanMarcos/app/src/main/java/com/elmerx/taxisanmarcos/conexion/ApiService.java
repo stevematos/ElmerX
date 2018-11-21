be0b2462368d084693d5578303cd302d13e7c734
juanSW18 <juan.eneque@unmsm.edu.pe>
2018-11-21 13:32:25 -0500

    Mejoras v3

--- Desarrollo/STSM/Desarrollo/FrontEnd/TaxiSanMarcos/app/src/main/java/com/elmerx/taxisanmarcos/conexion/ApiService.java
+++ Desarrollo/STSM/Desarrollo/FrontEnd/TaxiSanMarcos/app/src/main/java/com/elmerx/taxisanmarcos/conexion/ApiService.java
@@ -3,8 +3,18 @@ package com.elmerx.taxisanmarcos.conexion;
 //import retrofit2.Call;
 import com.elmerx.taxisanmarcos.modelo.Usuario;
 
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
 public interface ApiService {
 
-    //@POST("/usuario/add")
-    //Call<Usuario> registrarUsuario();
+    @POST("/usuario/add")
+    @Headers({"Accept: application/json"})
+    Call<Usuario> registrarUsuario(@Body Usuario request);
+
+    /*@POST("/usuario/update")
+    @Headers({"Accept: application/json"})
+    Call<Usuario> updateUsuario(@Body Usuario request);*/
 }
